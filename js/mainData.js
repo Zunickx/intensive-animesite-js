1303c84db801df78c81ac4dab8f2a865febc77b6
Zunickx <Zunickx@gmail.com>
2024-03-03 18:59:41 +0300

    Change file mainData.js: filtred by views

--- js/mainData.js
+++ js/mainData.js
@@ -5,11 +5,14 @@ const mainData = () => {
         })
         .then ((data) => {
             // console.log(data);// передача распакованных данных
-            const filterDate = data.sort((a, b) => {
-                return b.rating - a.rating;
-            }).filter((item) => item.rating >= 9.0); // отсортировали массив по рейтингу аниме
-
-            console.log(filterDate);
+            // const filterDate = data.sort((a, b) => {
+            //     return b.rating - a.rating;
+            // }).filter((item) => item.rating >= 9.0); // отсортировали массив по рейтингу аниме
+            const filterViews = data.sort((a, b) => {
+                return b.views - a.views;
+            })
+            // console.log(filterDate);
+            console.log(filterViews);
         })
 }
 
