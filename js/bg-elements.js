3a0d239367b1344280d9ada5b218cf2805e8731b
Zunickx <Zunickx@gmail.com>
2024-03-03 18:49:57 +0300

    Change file bg-elemrnts.js, mainData.js: отработка методов работы с массивами и массивами объектов

--- js/bg-elements.js
+++ js/bg-elements.js
@@ -11,14 +11,47 @@ const bgElements = () => {
     // }
 
     elements.forEach((elem) => {
-        console.log(elem);
+        // console.log(elem);
         const src = elem.dataset.setbg
         elem.style.backgroundImage = `url(${src})`;
 
         // либо это можно записать вот так
         // elem.style.backgroundImage = `url(${elem.dataset.setbg})`;
-
     })
+
+
+    // методы перебора массива и массива объектов
+    // const array = [4, 2, 5, 3, 1];
+
+    // const newArray = array.filter((item) => {
+    //     return item >= 3;
+    // }).sort();
+
+    // const newArray2 = array.sort()
+
+    // console.log(newArray);
+    // console.log(newArray2);
+
+    // const array3 = [
+    //     {
+    //         id: 0,
+    //         value: 100
+    //     },
+    //     {
+    //         id: 2,
+    //         value: 300
+    //     },
+    //     {
+    //         id: 1,
+    //         value: 200
+    //     }
+    // ]
+
+    // const objectArray = array3.sort((a, b) => {
+    //     return a.value - b.value
+    // });
+    // console.log(objectArray);
+
 }
 
 bgElements();
--- js/mainData.js
+++ js/mainData.js
@@ -4,7 +4,12 @@ const mainData = () => {
             return response.json() // распаковываем данные с сервера
         })
         .then ((data) => {
-            console.log(data) // передача распакованных данных
+            // console.log(data);// передача распакованных данных
+            const filterDate = data.sort((a, b) => {
+                return b.rating - a.rating;
+            }).filter((item) => item.rating >= 9.0); // отсортировали массив по рейтингу аниме
+
+            console.log(filterDate);
         })
 }
 
